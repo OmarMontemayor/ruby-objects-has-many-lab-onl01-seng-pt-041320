ed7b931ea51b3c6b5f876c90e3097372e398b09d
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-10 20:27:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,14 +11,9 @@ class Artist
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@song_count += 1
   end
   
   def songs
     @songs
   end
-  
-  def self.song_count
-    @@song_count
-  end  
 end
