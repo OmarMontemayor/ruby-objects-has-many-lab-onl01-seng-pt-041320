774e2e34ae17a8711d5ce1d53db6d84ec6df253b
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-10 20:51:56 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,17 @@
 class Song
   attr_accessor :title, :artist
+  
+  @@all = []
   def initialize(title, artist = nil)
     @title = title
     
   end
   
   def artist_name
+    
+  end
+  
+  def self.all
+    @@all
+  end
 end
